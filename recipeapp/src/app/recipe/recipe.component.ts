f9e905b1b4cb84b341e665443d2efb09c9b4287e
Thomas Duchatelet <thomas.duchatelet2@gmail.com>
2020-05-11 10:50:10 +0200

    added ingredient component

--- recipeapp/src/app/recipe/recipe.component.ts
+++ recipeapp/src/app/recipe/recipe.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+  name: string;
+  ingredients: string[];
+  dateAdded: Date;
 
-  constructor() { }
+  constructor() {
+    this.name = 'spaghetti';
+    this.ingredients = ['tomato', 'onion', 'celery', 'carrot', 'minced meat'];
+    this.dateAdded = new Date();
+   }
 
   ngOnInit(): void {
   }
